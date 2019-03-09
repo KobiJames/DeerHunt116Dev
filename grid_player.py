e42b3c53db5394a78c16d920c47059cd3ac59046
KobiJames <47069052+KobiJames@users.noreply.github.com>
2019-03-08 21:50:32 -0500

    Update grid_player.py

--- grid_player.py
+++ grid_player.py
@@ -3,7 +3,45 @@ class GridPlayer:
     def __init__(self):
         self.resourses_pos = []
         self.closest_resourse = ()
-
+    def distances(u):
+        l = []
+        ene = u.nearby_enemies_by_distance()
+        for i in ene:
+            l.append(i[1])
+        return l
+    
+    def attack_formation():
+        #organize warriors into attack formation
+        pass
+    def commandcenter():
+        #organize resources/other maintanence shit
+        pass
+    
+    def safe(u):# -> Moves or None #returns a valid move away from enemy:
+        ene = distances(u)
+        if 1 in ene:
+            r = u.position
+            for x in range(-1,2,2):
+                u.x += x
+                if (1 in dist) or (0 in dist):
+                    u.x -= x
+                else:
+                    if x is -1:
+                        return u.move(u.direction("DOWN"))
+                    else:
+                        return u.move(u.direction("UP"))
+            for y in range(-1,2,2):
+                u.y += y
+                dist = distances(u)
+                if (1 in dist) or (0 in dist):
+                    u.y -= y
+                else:
+                    if y is -1: 
+                        return u.move(u.direction("LEFT"))
+                    else:
+                        return u.move(u.direction("RIGHT"))
+        else: 
+            return None
     def tick(self, game_map, your_units, enemy_units, resources, turns_left):
         ans = []
         get_closest_resopurse()
